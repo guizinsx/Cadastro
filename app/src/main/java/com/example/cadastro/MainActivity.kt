0ac7bdeba6d75c9f819e9969c2e45a59d2f6a9b7
guizinsx <guizin51@gmail.com>
2024-03-04 12:57:23 -0300

    view binding True e criando o ActivityMain

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.cadastro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cadastro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var amb: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        amb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(amb.root)
     }
 }
